f27561e2a73c3b685fb4248e176c646e525ed69d
charlicarp <carlosacosta24@hotmail.com>
2022-10-07 17:50:03 -0300

    botones sin funcionar

--- style.js
+++ style.js
@@ -2,11 +2,13 @@ let readMore_btn = document.getElementById('readMore_btn');
 let hideText = document.getElementById('hideText');
 
 
+
 readMore_btn.addEventListener('click', toggleText);
 
 function toggleText() {
   hideText.classList.toggle('showText');
- 
+  aceiteUno.classList.toggle('showText');
+
 
   if(hideText.classList.contains('showText')) {
     readMore_btn.innerHTML = 'Leer menos'
@@ -14,5 +16,4 @@ function toggleText() {
   else {
     readMore_btn.innerHTML = 'Leer mas'
   }
-
 }
